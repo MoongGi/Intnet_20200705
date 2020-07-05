8ad053341dbe95751563eea6eb25d36654bf8e2c
김문기 <heartboy08@nate.com>
2020-07-05 12:47:02 +0900

    인텐트3 -> 데이터전달 -> 데이터 가져오기

--- app/src/main/java/kr/co/tjoeun/intnet_20200705/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intnet_20200705/SecondActivity.kt
@@ -2,11 +2,22 @@ package kr.co.tjoeun.intnet_20200705
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        // intent : 이 화면에 진입할때 사용한 Intent 객체
+        // getStringExrea : 우리가 첨부한 데이터가 String 이니 이를 뽑아내자.
+        // message : 보내줄떄 붙여준 이름표 (찾아낼 기준)
+
+        val receiveMessage = intent.getStringExtra("message")
+
+        // 텍스트뷰에 반영
+        messageTxt.text = receiveMessage
+
     }
 }
