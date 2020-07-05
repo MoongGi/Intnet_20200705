3ee79201f0a1b2ad506dfd606bac6bf3f96a6d0c
김문기 <heartboy08@nate.com>
2020-07-05 15:17:33 +0900

    SENDTO 기능

--- app/src/main/java/kr/co/tjoeun/intnet_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intnet_20200705/MainActivity.kt
@@ -1,11 +1,14 @@
 package kr.co.tjoeun.intnet_20200705
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -50,6 +53,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        //전화걸기 다이얼
+        callBtn.setOnClickListener {
+            //입력된 전화번호를 받아오자.
+
+            val inputPhonNum = phoneNumEdt.text.toString()
+
+            // 안드로이드 에게, 어디에 전화걸지 정보 전달 => URL
+            // 전화 URL양식 - tel:010-5112-3237
+        /*    val myUrl = Uri.parse("tel:${inputPhonNum}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUrl)
+                startActivity(myIntent)*/
+        }
+
+        sendSmsBtn.setOnClickListener {
+            //입력한 폰번호 / 문자 내용 받아오기
+            val inputPhone = phoneNumEdt.text.toString()
+            val inputMsg = smsContentEdt.text.toString()
+            val myUrl = Uri.parse("smsto:${inputPhone}")
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUrl)
+            myIntent.putExtra("sms_body",inputMsg)
+            startActivity(myIntent)
+        }
     }
 
     // MainActivity로 복귀하는 모든 시점에 실행되는 함수
